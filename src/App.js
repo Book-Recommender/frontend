92f41a03d39096fe3daf8ae8f62c1ccabd3a1c05
tun24709 <tun24709@temple.edu>
2024-11-12 21:50:52 -0500

    Added information and fixed file error bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles.css'
 import React from 'react';
 import LoginSignup from './components/loginSigninFiles/LoginSignup'
 
--- src/components/loginSigninFiles/LoginSignup.jsx
+++ src/components/loginSigninFiles/LoginSignup.jsx
@@ -85,36 +85,49 @@ const LoginSignup = () => {
 
 
     return(
-        <div className="container">
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
+        <body>
+            <div class="left-side">
+                <h1>Book Club</h1>
+                <p>Welcome to Book Club! Book Club was created by readers, for readers, to give personalized recommendations based on readers like you.</p>
+                <p>Please make an account using our sign up on the right, and let's get started!</p>
             </div>
-            <div className="inputs">
-                {action === "Login" ? <div></div>
-                    :
+            <div className="container">
+                <div className="header">
+                    <div className="text">{action}</div>
+                    <div className="underline"></div>
+                </div>
+                <div className="inputs">
+                    {action === "Login" ? <div></div>
+                        :
+                        <div className="input">
+                            <img src={userIcon} alt=""/>
+                            <input type="text" placeholder="Username"/>
+                        </div>
+                    }
                     <div className="input">
-                    <img src={userIcon} alt=""/>
-                    <input type="text" placeholder="Username"/>
+                        <img src={emailIcon} alt=""/>
+                        <input type="email" placeholder="Email"/>
                     </div>
+                    <div className="input">
+                        <img src={passwordIcon} alt=""/>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                </div>
+                {action === "Sign Up" ? <div></div>
+                    : <div className="forgot-password">Lost password? <span>Click here!</span></div>
                 }
-                <div className="input">
-                    <img src={emailIcon} alt=""/>
-            <input type="email" placeholder="Email"/>
-        </div>
-    <div className="input">
-        <img src={passwordIcon} alt=""/>
-                    <input type="password" placeholder="Password"/>
+                <div className="submit-container">
+                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => {
+                        setAction("Sign Up")
+                    }}>Sign Up
+                    </div>
+                    <div className={action === "Sign Up" ? "submit gray:" : "submit"} onClick={() => {
+                        setAction("Login")
+                    }}>Login
+                    </div>
                 </div>
             </div>
-            {action === "Sign Up" ? <div></div>
-                : <div className="forgot-password">Lost password? <span>Click here!</span></div>
-            }
-            <div className="submit-container">
-                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray:":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-            </div>
-        </div>
+        </body>
     )
 }
 
