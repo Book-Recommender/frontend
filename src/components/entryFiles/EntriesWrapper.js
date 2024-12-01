b1d11793712ad64a315facd6fec4a47a06945efd
tun24709 <tun24709@temple.edu>
2024-12-01 14:55:19 -0500

    fixing merge conflicts

--- src/components/entryFiles/EntriesWrapper.js
+++ src/components/entryFiles/EntriesWrapper.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
-import {EntriesForm} from "./EntriesForm";
-import {v4 as uuidv4} from 'uuid';
-import {Entries} from "./Entries";
-import {EditEntries} from "./EditEntries";
+import React, { useState } from 'react';
+import { EntriesForm } from "./EntriesForm";
+import { v4 as uuidv4 } from 'uuid';
+import { Entries } from "./Entries";
+import { EditEntries } from "./EditEntries";
+import './bookEntry.css';
+
 uuidv4();
 
 export const EntriesWrapper = () => {
